35690adef0db055db2b5777c0e95098c7a8da0dd
jvf@home <web@jens-fischer.eu>
2012-06-15 18:43:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ruby on Rails Tutorial
+
+This Repository holds my first Rails Applications build during the Rails Tutorial
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f53927f8e80f4cb9d6991dd12f50984a4b00a37327ae38f0d4dd3f2e64f1b37575e6dacbc07690ce3375fbc3e047f976e409b12e32bd8a812edfc6cca945474e'
