d44f17076444916c85d9332f8c4f3052746b199c
jvf@home <web@jens-fischer.eu>
2012-06-29 22:21:51 +0200

    Add flash message to user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,10 @@ class UsersController < ApplicationController
   def create
    @user = User.new(params[:user])
     if @user.save
-     redirect_to @user 
-    else
-      render 'new'
+       flash[:success] = "Welcome to the Sample App!"
+       redirect_to @user 
+      else
+        render 'new'
     end
   end
 end
