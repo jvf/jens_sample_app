4da4887f3f4e4033a0ba5336dd645301167e1e7e
jvf@home <web@jens-fischer.eu>
2012-06-29 16:51:44 +0200

    Add user resource to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+
+  resources :users
+
+  root :to => 'static_pages#home'
 
 #  get "static_pages/home"
   match '/signup', to: 'users#new'
@@ -56,7 +59,6 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
